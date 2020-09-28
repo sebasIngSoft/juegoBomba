4710eb2a2cc04b75475d380f96a8f33cabf4f129
Sebastian Hidalgo <sebashidalgo1995@gmail.com>
2020-09-27 20:11:56 -0500

    Ya cunciona totalmente
    solo falta agregar los sonidos

--- app/src/main/java/com/example/juegobomba/MainActivity.java
+++ app/src/main/java/com/example/juegobomba/MainActivity.java
@@ -2,13 +2,181 @@ package com.example.juegobomba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        clsAsincrono tarea1;
+        clsAsincrono tarea2;
+
+        TextView progressBom1;
+        TextView progressBom2;
+
+        Button btnNegro1,btnNegro2,btnRojo1,btnRojo2,btnAzul1,btnAzul2;
+
+        ImageView imgDesactivado1, imgDesactivado2;
+
+        FrameLayout frmInicial,frmExplocion,frmGanaste,frmBtnBomba1,frmBtnBomba2;
+
+        private boolean validar1,validar2;
+        int bomba1;
+        int bomba2;
+        
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            progressBom1 = (TextView) findViewById(R.id.progressBom1);
+            progressBom2 = (TextView) findViewById(R.id.progressBom2);
+
+            btnNegro1 = (Button) findViewById(R.id.btnNegro1);
+            btnNegro2 = (Button) findViewById(R.id.btnNegro2);
+            btnRojo1 = (Button) findViewById(R.id.btnRojo1);
+            btnRojo2 = (Button) findViewById(R.id.btnRojo2);
+            btnAzul1 = (Button) findViewById(R.id.btnAzul1);
+            btnAzul2 = (Button) findViewById(R.id.btnAzul2);
+
+            imgDesactivado1 = (ImageView) findViewById(R.id.imgDesactivado1);
+            imgDesactivado2 = (ImageView) findViewById(R.id.imgDesactivado2);
+
+            frmInicial = (FrameLayout) findViewById(R.id.frmInicial);
+            frmExplocion = (FrameLayout) findViewById(R.id.frmExplocion);
+            frmGanaste = (FrameLayout) findViewById(R.id.frmGanaste);
+            frmBtnBomba1 = (FrameLayout) findViewById(R.id.frmBtnBomba1);
+            frmBtnBomba2 = (FrameLayout) findViewById(R.id.frmBtnBomba2);
+
+            validar1=false;
+            validar2=false;
+
+            bomba1=(int)(Math.random()*3)+1;
+            bomba2=(int)(Math.random()*3)+1;
+
+            System.out.println("---------------------------BOMBA 1 ="+bomba1+" y BOMBA 2 ="+bomba2);
+
+            clickHilosMultiples();
+        }
+
+        public void reiniciar(View view){
+            Intent intent = new Intent(this , MainActivity.class);
+            //llamamos a la actividad
+            startActivity(intent);
+            //finalizamos la actividad actual
+            finish();
+           /* frmGanaste.setVisibility(View.GONE);
+            frmExplocion.setVisibility(View.GONE);
+            frmInicial.setVisibility(View.VISIBLE);
+            frmBtnBomba1.setVisibility(View.VISIBLE);
+            frmBtnBomba2.setVisibility(View.VISIBLE);
+            imgDesactivado1.setVisibility(View.GONE);
+            imgDesactivado2.setVisibility(View.GONE);
+            clickHilosMultiples();*/
+        }
+
+        public void DetenerHilo(View view){
+            Button boton = view.findViewById(view.getId());
+
+                if(boton==btnNegro1 || boton==btnRojo1 || boton==btnAzul1){
+                    validar1=validarBomba1(boton);
+                    /*llamo el metodo para saber si ya desactivo las dos bombas y gano*/
+                    isGanador();
+                }else{
+                    validar2=validarBomba2(boton);
+                    isGanador();
+                }
+        }
+        /*Se detiene el hilo*/
+        public void clickCancelarClaseAsincrona(clsAsincrono tarea) {
+            tarea.cancel(true);
+        }
+
+        public boolean validarBomba1(Button boton){
+            if (bomba1==1 && boton == btnNegro1){
+                imgDesactivado1.setVisibility(View.VISIBLE);
+                frmBtnBomba1.setVisibility(View.INVISIBLE);
+                clickCancelarClaseAsincrona(tarea1);
+                return true;
+            }else if(bomba1 == 2 &&  boton == btnRojo1){
+                imgDesactivado1.setVisibility(View.VISIBLE);
+                frmBtnBomba1.setVisibility(View.INVISIBLE);
+                clickCancelarClaseAsincrona(tarea1);
+                return true;
+            }else if(bomba1 == 3 && boton == btnAzul1){
+                imgDesactivado1.setVisibility(View.VISIBLE);
+                frmBtnBomba1.setVisibility(View.INVISIBLE);
+                clickCancelarClaseAsincrona(tarea1);
+                return true;
+            }else{
+                frmExplocion.setVisibility(View.VISIBLE);
+                frmInicial.setVisibility(View.GONE);
+                return false;
+            }
+        }
+
+        public boolean validarBomba2(Button boton){
+            if (bomba2==1 && boton == btnNegro2){
+                imgDesactivado2.setVisibility(View.VISIBLE);
+                frmBtnBomba2.setVisibility(View.INVISIBLE);
+                clickCancelarClaseAsincrona(tarea2);
+                return true;
+            }else if(bomba2 == 2 &&  boton == btnRojo2){
+                imgDesactivado2.setVisibility(View.VISIBLE);
+                frmBtnBomba2.setVisibility(View.INVISIBLE);
+                clickCancelarClaseAsincrona(tarea2);
+                return true;
+            }else if(bomba2 == 3 && boton == btnAzul2){
+                imgDesactivado2.setVisibility(View.VISIBLE);
+                frmBtnBomba2.setVisibility(View.INVISIBLE);
+                clickCancelarClaseAsincrona(tarea2);
+                return true;
+            }else{
+                frmExplocion.setVisibility(View.VISIBLE);
+                frmInicial.setVisibility(View.GONE);
+                return false;
+            }
+        }
+
+        public void isGanador(){
+            if (validar1==true && validar2==true){
+                frmInicial.setVisibility(View.GONE);
+                frmGanaste.setVisibility(View.VISIBLE);
+            }
+        }
+
+        /**************END USO DE LA CLASE ASYNCTASK, OPCION RECOMENDADA*****************************/
+
+
+        public void clickHilosMultiples(){
+
+            try {
+                tarea1.cancel(true);
+                tarea2.cancel(true);
+            }catch (Exception e){}
+
+            /*Se definen los hilos*/
+            tarea1 = new clsAsincrono(MainActivity.this, progressBom1,frmInicial,frmExplocion);
+            tarea2 = new clsAsincrono(MainActivity.this, progressBom2,frmInicial,frmExplocion);
+
+            /*Se valida si la version es mayor a honeycomb (3.0)*/
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                /*Si es mayor lo metemos al pool de hilos, (Son maximo 5)*/
+                /*executeOnExecutor(PoolDeHilos, parametros si se desean mandar)*/
+                tarea1.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,null);
+                tarea2.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,null);
+            } else {
+                /*Si no ejecute los hilos normalmente.*/
+                tarea1.execute();
+                tarea2.execute();
+            }
+        }
     }
-}
---
+++ app/src/main/java/com/example/juegobomba/clsAsincrono.java
@@ -0,0 +1,152 @@
+package com.example.juegobomba;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Johnny on 21/06/2019.
+ */
+
+/*
+onPreExecute(). Se ejecutará antes del código principal de nuestra tarea. Se suele utilizar para
+                preparar la ejecución de la tarea, inicializar la interfaz, etc.
+
+doInBackground(). Contendrá el código principal de nuestra tarea.
+
+onProgressUpdate(). Se ejecutará cada vez que llamemos al método publishProgress() desde el método
+                    doInBackground().
+
+onPostExecute(). Se ejecutará cuando finalice nuestra tarea, o dicho de otra forma, tras la
+                 finalización del método doInBackground().
+
+onCancelled(). Se ejecutará cuando se cancele la ejecución de la tarea antes de su finalización
+               normal.
+*/
+
+
+/*Los parametros que estamos recibiendo al heredar del AsyncTask son:
+
+      Parametro 1: El tipo de datos que recibiremos como entrada de la tarea en el método
+                   doInBackground(). En este caso doInBackground() no recibirá ningún parámetro
+                   de entrada (Void).
+      Parametro 2: El tipo de datos con el que actualizaremos el progreso de la tarea, y que
+                   recibiremos como parámetro del método onProgressUpdate() y que a su vez
+                   tendremos que incluir como parámetro del método publishProgress(). En este
+                   caso publishProgress() y onProgressUpdate() recibirán como parámetros datos de
+                   tipo entero (Integer).
+      Parametro 3: El tipo de datos que devolveremos como resultado de nuestra tarea, que será el
+                   tipo de retorno del método doInBackground() y el tipo del parámetro recibido en
+                   el método onPostExecute(). En este caso doInBackground() devolverá como retorno
+                   un dato de tipo booleano y onPostExecute() también recibirá como parámetro un
+                   dato del dicho tipo (Boolean).
+*/
+public class clsAsincrono extends AsyncTask<Void, Integer, Boolean> {
+    TextView lblCrono;
+    private Activity activity;
+    private FrameLayout frmInicial,frmExplocion;
+    /*Variables globales con las cuales implemento los minutos y los segundos*/
+    int minutes = 0;
+    int seconds = 60;
+
+    /*Constructor*/
+    public clsAsincrono(Activity activity, TextView lblCrono,FrameLayout frmInicial,FrameLayout frmExplocion) {
+        this.activity = activity;
+        this.lblCrono = lblCrono;
+        this.frmInicial = frmInicial;
+        this.frmExplocion = frmExplocion;
+    }
+
+    private void detenerHilo()
+    {
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {}
+    }
+
+
+    /*Antes de iniciar el proceso, reiniciamos los cronometros*/
+    @Override
+    protected void onPreExecute() {
+        /*Antes de iniciar el proyecto le asigno el valor a la variable minutos*/
+        /*Le seteo los valores al label de la ventana principal*/
+        minutes = 2;
+        String timeCrono;
+        timeCrono = " "+minutes;
+        timeCrono += " : 00";
+        lblCrono.setText(timeCrono);
+    }
+
+    /*
+    * El método doInBackground() se ejecuta en un hilo secundario (por tanto no podremos interactuar
+    * con la interfaz), pero sin embargo todos los demás se ejecutan en el hilo principal, lo que
+    * quiere decir que dentro de ellos podremos hacer referencia directa a nuestros controles de
+    * usuario para actualizar la interfaz. Ademas si se llama al metodo publishProgress() este
+    * automáticamente ejecuta el onProgressUpdate() que actualizara la interfaz si es necesario
+    * */
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        /*Empieza en 0 para poder hacer que i siempre sea 1 y no se le aplica un auto incrementable*/
+        for(int i=0; i<=minutes;) {
+            detenerHilo();
+            /*Si los segundos son iguales a 60 disminuye el valor de la variable minutes*/
+            if(seconds==60)minutes-=1;
+            /*Se llama a la funcion onProgressUpdate para actualizar la interfaz*/
+            /*Siempre se le va a enviar i+1 ya que i siempre va a ser cero*/
+            publishProgress(i+1);
+            /*Se valida si se ha solicitado finalizar el proceso*/
+            if(isCancelled())
+                /*Se llama a la funcion onCancelled*/
+                break;
+        }
+
+        /*Se le retorna true a la funcion onPostExecute*/
+        /*Si no se cumple el for, se le asignan nuevos valores a minutes y seconds para finalizar el contador.*/
+        minutes = 0;
+        seconds = 1;
+        return true;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        /*Se saca el progreso mandado y se actualiza el valor de la variable seconds restandole el progreso resivido en el método*/
+        seconds = seconds-(values[0].intValue());
+        String timeCrono;
+        timeCrono = minutes+" : ";
+        timeCrono+=seconds;
+        if(seconds==0){
+            if(minutes==0){
+                minutes-=1;
+            }else{
+                seconds=60;
+            }
+        }
+        lblCrono.setText(timeCrono);
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        /*Tan pronto termine el proceso, se muestra un toast en la activity
+        indincando que termino
+        * el proceso*/
+        if(result){
+            frmInicial.setVisibility(View.GONE);
+            frmExplocion.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
+
+    @Override
+    protected void onCancelled() {
+        /*Si se cancela el proceso se le indica al usuario*/
+        Toast.makeText(activity, "Tarea cancelada!",
+                Toast.LENGTH_SHORT).show();
+    }
+}
